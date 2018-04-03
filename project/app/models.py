ee5222521416cd4b5f7702e9516c74921ddf1455
Alex Clark <aclark@aclark.net>
2018-04-03 07:52:25 -0400

    Update

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+import recurrence.fields
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    start = models.TimeField()
+    end = models.TimeField()
+    recurrences = recurrence.fields.RecurrenceField()
